fbf9152ca9c7d6ab0a7ab23b117ad45647d69bc8
Qian Hu <qianhu299792458@gmail.com>
2023-11-19 11:20:39 +0000

    add tests

--- river/models/utils.py
+++ river/models/utils.py
@@ -1,5 +1,13 @@
 import numpy as np 
 import pickle 
+import matplotlib.pyplot as plt 
+import bilby
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torch import nn
+import pandas as pd
+from .embedding.pca import project_strain_data_FDAPhi
+
 
 def save_model(filename, model):
     with open(filename, 'wb') as f:
@@ -11,3 +19,195 @@ def load_model(filename):
         model = pickle.load(f)
     print(f'Model loaded from {filename}')
     return model 
+
+def adjust_lr(optimizer, gamma):
+    for g in optimizer.param_groups:
+        g['lr'] = g['lr']*gamma
+
+def save_loss_data(train_losses, valid_losses, outdir, logscale='true'):
+    plt.figure(figsize=(8,5))
+    plt.plot(train_losses, label='train')
+    plt.plot(valid_losses, label='valid')
+    plt.legend()
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+
+    if logscale:
+        plt.yscale('log')
+    
+    plt.savefig(f'{outdir}/losses.png')
+    np.savetxt(f'{outdir}/train_losses.txt', train_losses)
+    np.savetxt(f'{outdir}/valid_losses.txt', valid_losses)
+
+
+def train_zukoflow(flow, embedding_proj, embedding_noproj, optimizer, dataloader, detector_names, ipca_gen, device='cpu',downsample_rate=1):
+    flow.train()
+    embedding_proj.train()
+    embedding_noproj.train()
+    loss_list = []
+    for theta, strain, psd in dataloader:
+        optimizer.zero_grad()
+
+        inputs_proj = project_strain_data_FDAPhi(strain, psd, detector_names, ipca_gen).to(device)
+        inputs_noproj = project_strain_data_FDAPhi(strain, psd, detector_names, ipca_gen, project=False).unsqueeze(1)[:,:,::downsample_rate].to(device)
+        theta = theta.to(device)
+
+        embedding_out_proj = embedding_proj(inputs_proj)
+        embedding_out_noproj = embedding_noproj(inputs_noproj)
+        condition = torch.cat((embedding_out_proj, embedding_out_noproj), -1)
+
+        loss = -flow(condition).log_prob(theta).mean() # mean(list of losses of elements in this batch)
+        loss.backward()
+        optimizer.step()
+
+        loss_list.append(loss.detach())
+
+    mean_loss = torch.stack(loss_list).mean().item() # mean(list of mean losses of each batch)
+    std_loss = torch.stack(loss_list).std().item()
+    return mean_loss, std_loss
+
+def eval_zukoflow(flow, embedding_proj, embedding_noproj, dataloader, detector_names, ipca_gen, device='cpu',downsample_rate=1):
+    flow.eval()
+    embedding_proj.eval()
+    embedding_noproj.eval()
+    loss_list = []
+    with torch.no_grad():
+        for theta, strain, psd in dataloader:
+
+            inputs_proj = project_strain_data_FDAPhi(strain, psd, detector_names, ipca_gen).to(device)
+            inputs_noproj = project_strain_data_FDAPhi(strain, psd, detector_names, ipca_gen, project=False).unsqueeze(1)[:,:,::downsample_rate].to(device)
+            theta = theta.to(device)
+
+            embedding_out_proj = embedding_proj(inputs_proj)
+            embedding_out_noproj = embedding_noproj(inputs_noproj)
+            condition = torch.cat((embedding_out_proj, embedding_out_noproj), -1)
+
+            loss = -flow(condition).log_prob(theta).mean()
+            loss_list.append(loss.detach())
+
+    mean_loss = torch.stack(loss_list).mean().item()
+    std_loss = torch.stack(loss_list).std().item()
+    return mean_loss, std_loss
+
+
+def sample_zukoflow(flow, embedding_proj, embedding_noproj, dataset, detector_names, ipca_gen, device='cpu', Nsample=5000, downsample_rate=1):
+    flow.eval()
+    #loss_list = []
+    #sample_list = []
+    dataloader = DataLoader(dataset, batch_size=len(dataset), shuffle=False)
+    with torch.no_grad():
+        for theta, strain, psd in dataloader:
+            inputs_proj = project_strain_data_FDAPhi(strain, psd, detector_names, ipca_gen).to(device)
+            inputs_noproj = project_strain_data_FDAPhi(strain, psd, detector_names, ipca_gen, project=False).unsqueeze(1)[:,:,::downsample_rate].to(device)
+            theta = theta.to(device)
+
+            embedding_out_proj = embedding_proj(inputs_proj)
+            embedding_out_noproj = embedding_noproj(inputs_noproj)
+            condition = torch.cat((embedding_out_proj, embedding_out_noproj), -1)
+
+            loss = -flow(condition).log_prob(theta)
+            samples = flow(condition).sample((Nsample,))
+
+            #loss_list.append(loss.detach())
+            #sample_list.append(samples)
+
+    return samples.movedim(0,1).movedim(1,2), loss
+
+
+def train_glasflow(flow, embedding_proj, embedding_noproj, optimizer, dataloader, detector_names, ipca_gen, device='cpu',downsample_rate=1):
+    flow.train()
+    embedding_proj.train()
+    embedding_noproj.train()
+    loss_list = []
+    for theta, strain, psd in dataloader:
+        optimizer.zero_grad()
+
+        inputs_proj = project_strain_data_FDAPhi(strain, psd, detector_names, ipca_gen).to(device)
+        inputs_noproj = project_strain_data_FDAPhi(strain, psd, detector_names, ipca_gen, project=False).unsqueeze(1)[:,:,::downsample_rate].to(device)
+        theta = theta.to(device)
+
+        embedding_out_proj = embedding_proj(inputs_proj)
+        embedding_out_noproj = embedding_noproj(inputs_noproj)
+        condition = torch.cat((embedding_out_proj, embedding_out_noproj), -1)
+
+        loss = -flow.log_prob(theta, conditional=condition).mean() # mean(list of losses of elements in this batch)
+        loss.backward()
+        optimizer.step()
+
+        loss_list.append(loss.detach())
+
+    mean_loss = torch.stack(loss_list).mean().item() # mean(list of mean losses of each batch)
+    std_loss = torch.stack(loss_list).std().item()
+    return mean_loss, std_loss
+
+def eval_glasflow(flow, embedding_proj, embedding_noproj, dataloader, detector_names, ipca_gen, device='cpu',downsample_rate=1):
+    flow.eval()
+    embedding_proj.eval()
+    embedding_noproj.eval()
+    loss_list = []
+    with torch.no_grad():
+        for theta, strain, psd in dataloader:
+
+            inputs_proj = project_strain_data_FDAPhi(strain, psd, detector_names, ipca_gen).to(device)
+            inputs_noproj = project_strain_data_FDAPhi(strain, psd, detector_names, ipca_gen, project=False).unsqueeze(1)[:,:,::downsample_rate].to(device)
+            theta = theta.to(device)
+
+            embedding_out_proj = embedding_proj(inputs_proj)
+            embedding_out_noproj = embedding_noproj(inputs_noproj)
+            condition = torch.cat((embedding_out_proj, embedding_out_noproj), -1)
+
+            loss = -flow.log_prob(theta, conditional=condition).mean()
+            loss_list.append(loss.detach())
+
+    mean_loss = torch.stack(loss_list).mean().item()
+    std_loss = torch.stack(loss_list).std().item()
+    return mean_loss, std_loss
+
+
+def sample_glasflow(flow, embedding_proj, embedding_noproj, dataset, detector_names, ipca_gen, device='cpu', Nsample=5000,downsample_rate=1):
+    flow.eval()
+    #loss_list = []
+    #sample_list = []
+    dataloader = DataLoader(dataset, batch_size=len(dataset), shuffle=False)
+    with torch.no_grad():
+        for theta, strain, psd in dataloader:
+            inputs_proj = project_strain_data_FDAPhi(strain, psd, detector_names, ipca_gen).to(device)
+            inputs_noproj = project_strain_data_FDAPhi(strain, psd, detector_names, ipca_gen, project=False).unsqueeze(1)[:,:,::downsample_rate].to(device)
+            theta = theta.to(device)
+
+            embedding_out_proj = embedding_proj(inputs_proj)
+            embedding_out_noproj = embedding_noproj(inputs_noproj)
+            condition = torch.cat((embedding_out_proj, embedding_out_noproj), -1)
+
+            loss = -flow.log_prob(theta, conditional=condition).mean()
+            samples = flow.sample(Nsample, conditional=condition)
+
+            #loss_list.append(loss.detach())
+            #sample_list.append(samples)
+
+    return samples.movedim(0,1).movedim(1,2), loss
+
+
+def make_results(sample_list, injection_parameters_list, parameter_names):
+    Nresult = len(sample_list)
+
+    result_list = []
+    for i in range(Nresult):
+        result = bilby.gw.result.CompactBinaryCoalescenceResult()
+        injection_parameters = {}
+        posterior_dict = {}
+        search_parameter_keys = []
+        parameter_labels_with_unit = []
+        for j,paraname in enumerate(parameter_names):
+            injection_parameters[paraname] = injection_parameters_list[paraname][i]
+            posterior_dict[paraname] = np.array(sample_list[i][j].cpu())
+            search_parameter_keys.append(paraname)
+            parameter_labels_with_unit.append(paraname)
+        
+        result.posterior = pd.DataFrame.from_dict(posterior_dict)
+        result.injection_parameters = injection_parameters
+        result.search_parameter_keys = search_parameter_keys
+        result.parameter_labels_with_unit = parameter_labels_with_unit
+        result_list.append(result)
+    
+    return result_list
--- scripts/train.py
+++ scripts/train.py
@@ -5,6 +5,7 @@ import sys
 import matplotlib.pyplot as plt
 
 import zuko
+from glasflow import RealNVP, CouplingNSF
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torch import nn
@@ -20,7 +21,7 @@ from river.data.dataset import DatasetStrainFD
 from river.data.utils import *
 
 from river.models import embedding
-from river.models import utils as modelutils
+from river.models.utils import *
 from river.models.embedding.pca import project_strain_data_FDAPhi
 from river.models.embedding.conv import EmbeddingConv1D, EmbeddingConv2D
 
@@ -35,65 +36,6 @@ formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
 stdout_handler = logging.StreamHandler(sys.stdout)
 stdout_handler.setLevel(logging.DEBUG)
 stdout_handler.setFormatter(formatter)
-
-def train_zukoflow(flow, embedding_proj, embedding_noproj, optimizer, dataloader, detector_names, ipca_gen, device='cpu',downsample_rate=1):
-    flow.train()
-    loss_list = []
-    for theta, strain, psd in dataloader:
-        optimizer.zero_grad()
-
-        inputs_proj = project_strain_data_FDAPhi(strain, psd, detector_names, ipca_gen).to(device)
-        inputs_noproj = project_strain_data_FDAPhi(strain, psd, detector_names, ipca_gen, project=False).unsqueeze(1).to(device)[:,:,::downsample_rate]
-        theta = theta.to(device)
-
-        embedding_out_proj = embedding_proj(inputs_proj)
-        embedding_out_noproj = embedding_noproj(inputs_noproj)
-        condition = torch.cat((embedding_out_proj, embedding_out_noproj), -1)
-
-        loss = -flow(condition).log_prob(theta).mean() # mean(list of losses of elements in this batch)
-        loss_list.append(loss.detach())
-        loss.backward()
-        optimizer.step()
-
-    mean_loss = torch.stack(loss_list).mean().item() # mean(list of mean losses of each batch)
-    std_loss = torch.stack(loss_list).std().item()
-    return mean_loss, std_loss
-
-def eval_zukoflow(flow, embedding_proj, embedding_noproj, dataloader, detector_names, ipca_gen, device='cpu',downsample_rate=1):
-    flow.eval()
-    loss_list = []
-    with torch.no_grad():
-        for theta, strain, psd in dataloader:
-
-            inputs_proj = project_strain_data_FDAPhi(strain, psd, detector_names, ipca_gen).to(device)
-            inputs_noproj = project_strain_data_FDAPhi(strain, psd, detector_names, ipca_gen, project=False).unsqueeze(1).to(device)[:,:,::downsample_rate]
-            theta = theta.to(device)
-
-            embedding_out_proj = embedding_proj(inputs_proj)
-            embedding_out_noproj = embedding_noproj(inputs_noproj)
-            condition = torch.cat((embedding_out_proj, embedding_out_noproj), -1)
-
-            loss = -flow(condition).log_prob(theta).mean()
-            loss_list.append(loss.detach())
-
-    mean_loss = torch.stack(loss_list).mean().item()
-    std_loss = torch.stack(loss_list).std().item()
-    return mean_loss, std_loss
-
-def save_loss_data(train_losses, valid_losses, outdir, logscale='true'):
-    plt.figure(figsize=(8,5))
-    plt.plot(train_losses, label='train')
-    plt.plot(valid_losses, label='valid')
-    plt.legend()
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-
-    if logscale:
-        plt.yscale('log')
-    
-    plt.savefig(f'{outdir}/losses.png')
-    np.savetxt(f'{outdir}/train_losses.txt', train_losses)
-    np.savetxt(f'{outdir}/valid_losses.txt', valid_losses)
 
 
 #logger.addHandler(stdout_handler)
@@ -167,7 +109,7 @@ valid_loader = DataLoader(dataset_valid, batch_size=batch_size_valid, shuffle=Tr
 
 
 
-ipca_gen = modelutils.load_model('ipca_models/IPCA_BNSFD_10000to500_ExpUnwrap_fixtc.pickle')
+ipca_gen = load_model('ipca_models/IPCA_BNSFD_10000to500_ExpUnwrap_fixtc.pickle')
 
 n_components = ipca_gen.n_components
 
@@ -177,16 +119,19 @@ device='cuda'
 
 embedding_proj = EmbeddingConv1D(ndet=3, ncomp=n_components, nout=128, middle_channel=1024).to(device)
 embedding_noproj = EmbeddingConv2D(ndet=3, ncomp=n_freq, nout=128, middle_channel=8).to(device)
+#flow = zuko.flows.NSF(features=17, context=256, transforms=100, hidden_features=(640, 640)).to(device)
+#flow = zuko.flows.CNF(features=17, context=256, hidden_features=(640, 640)).to(device)
+flow = CouplingNSF(n_inputs=17,n_transforms=100, n_conditional_inputs=256, n_neurons=128, batch_norm_between_transforms=True,).to(device)
 
+train_func = train_glasflow #train_zukoflow
+eval_func = eval_glasflow #eval_zukoflow
+lr = 1e-3
+gamma = 0.5
+optimizer = torch.optim.Adam(list(embedding_proj.parameters()) + list(embedding_noproj.parameters()) + list(flow.parameters()), lr=lr)
+#sche_step_size = 20
+#scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=sche_step_size, gamma=gamma)
 
-#flow = zuko.flows.NSF(features=17, context=128, transforms=100, hidden_features=(640, 640)).to(device)
-flow = zuko.flows.CNF(features=17, context=256, hidden_features=(640, 640)).to(device)
-
-
-optimizer = torch.optim.Adam(list(embedding_proj.parameters()) + list(embedding_noproj.parameters()) + list(flow.parameters()), lr=5e-4)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.7)
-
-ckpt_dir = 'trained_models/cnf_fixtc_twoconv'
+ckpt_dir = 'trained_models/glasnsf_fixtc_twoconv'
 if not os.path.exists(ckpt_dir):
     os.mkdir(ckpt_dir)
     print(f"Made dir {ckpt_dir}")
@@ -198,20 +143,25 @@ file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 ckpt_path = f'{ckpt_dir}/checkpoint.pickle'
 
-max_epoch = 200000
-epoches_update = 100
+max_epoch = 2000000
+epoches_update = 20
+epoches_pretrain = 40
+epoches_save_loss = 20
+epoches_adjust_lr = 20
 
-load_from_previous_train = 0
+load_from_previous_train = 1
 if load_from_previous_train:
     checkpoint = torch.load(ckpt_path)
     
-    start_epoch = checkpoint['epoch']
+    best_epoch = checkpoint['epoch']
+    start_epoch = best_epoch + 1
+    lr_updated_epoch = start_epoch
     embedding_proj.load_state_dict(checkpoint['embd_proj_state_dict'])
     embedding_noproj.load_state_dict(checkpoint['embd_noproj_state_dict'])
     flow.load_state_dict(checkpoint['flow_state_dict']) 
 
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-    scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+    #scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
     
 
     train_losses = checkpoint['train_losses']
@@ -222,20 +172,24 @@ else:
     train_losses = []
     valid_losses = []
     start_epoch = 0
+    lr_updated_epoch = start_epoch
 
+
+for g in optimizer.param_groups:
+    g['lr'] = 5e-4
 
 for epoch in range(start_epoch, max_epoch):    
-    if epoch % epoches_update == 0 and epoch!=0:
+    if epoch % epoches_update == 0 and epoch>=epoches_pretrain:
         data_generator_train.initialize_data()
         injection_parameters_train = generate_BNS_injection_parameters(Nsample, a_max=0.1, d_min=10, d_max=100, d_power=3, tc_min=-0.1, tc_max=0.1)
         data_generator_train.inject_signals(injection_parameters_train, Nsample)
         data_generator_train.numpy_starins()
         dataset_train = DatasetStrainFD(data_dict=data_generator_train.data, parameter_names=PARAMETER_NAMES_PRECESSINGBNS_BILBY)
-        train_loader = DataLoader(dataset_train, batch_size=batch_size, shuffle=True)
+        train_loader = DataLoader(dataset_train, batch_size_train, shuffle=True)
         logger.info(f"Training data updated at epoch={epoch}")
 
-    train_loss, train_loss_std = train_zukoflow(flow, embedding_proj, embedding_noproj, optimizer, train_loader, detector_names, ipca_gen, device=device, downsample_rate=downsample_rate)
-    valid_loss, valid_loss_std = eval_zukoflow(flow,  embedding_proj, embedding_noproj, valid_loader, detector_names, ipca_gen, device=device, downsample_rate=downsample_rate)
+    train_loss, train_loss_std = train_func(flow, embedding_proj, embedding_noproj, optimizer, train_loader, detector_names, ipca_gen, device=device, downsample_rate=downsample_rate)
+    valid_loss, valid_loss_std = eval_func(flow,  embedding_proj, embedding_noproj, valid_loader, detector_names, ipca_gen, device=device, downsample_rate=downsample_rate)
 
     train_losses.append(train_loss)
     valid_losses.append(valid_loss)
@@ -249,15 +203,17 @@ for epoch in range(start_epoch, max_epoch):
             'embd_proj_state_dict': embedding_proj.state_dict(),
             'embd_noproj_state_dict': embedding_noproj.state_dict(),
             'flow_state_dict': flow.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            'scheduler_state_dict': scheduler.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(), #'scheduler_state_dict': scheduler.state_dict(),
             'train_losses': train_losses,
             'valid_losses': valid_losses
             }, ckpt_path)
 
         logger.info(f'Current best epoch: {best_epoch}. Checkpoint saved.')
 
-    if epoch%20 == 0 and epoch!=0:
+    if epoch%epoches_save_loss == 0 and epoch!=0:
         save_loss_data(train_losses, valid_losses, ckpt_dir)
     
-    scheduler.step()
+    if epoch-best_epoch>epoches_adjust_lr and epoch-lr_updated_epoch>5:
+        adjust_lr(optimizer, gamma)
+        logger.info(f'Validation loss has not dropped for {epoches_adjust_lr} epoches. Learning rate decreased by a factor of {gamma}.')
+    #scheduler.step()
