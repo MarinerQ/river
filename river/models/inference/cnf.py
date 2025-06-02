02c8232cc5f3462e91568f8b9e999fdbfe93c869
Qian Hu <qianhu299792458@gmail.com>
2025-06-02 06:00:11 -0700

    remove zuko

--- river/models/inference/cnf.py
+++ river/models/inference/cnf.py
@@ -3,7 +3,6 @@ from torch import nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 
-import zuko
 from glasflow import RealNVP, CouplingNSF
 
 from ..utils import get_model, project_strain_data_FDAPhi
